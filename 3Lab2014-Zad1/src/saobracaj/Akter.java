8a697a74671faf360087e0f1aa02fae5195f0c3f
Luka <lukapetrovicsi@gmail.com>
2014-05-15 01:39:57 +0200

    Update Akter.java

--- 3Lab2014-Zad1/src/saobracaj/Akter.java
+++ 3Lab2014-Zad1/src/saobracaj/Akter.java
@@ -25,6 +25,6 @@ public abstract class Akter extends Thread {
 	public synchronized void kreni(){
 		radi = true; notifyAll();
 	}
-	public void zavrsi(){ interrupt(); System.out.println("Al sam ga prekinuo"); }
+	public void zavrsi(){ interrupt(); }
 	
 }
