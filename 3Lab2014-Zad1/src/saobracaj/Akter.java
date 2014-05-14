c1bd131377a33396e431a2b0986c710ac25b52af
Luka <lukapetrovicsi@gmail.com>
2014-05-15 01:48:04 +0200

    Update Akter.java

--- 3Lab2014-Zad1/src/saobracaj/Akter.java
+++ 3Lab2014-Zad1/src/saobracaj/Akter.java
@@ -10,15 +10,15 @@ public abstract class Akter extends Thread {
 	public abstract void radnja() throws InterruptedException;
 	public void run(){
 		try{
-			while (!interrupted()){
-				if (!radi) synchronized (this) {
-					while(!radi) wait();
-				}
-				System.out.println("Sacu da krenem da radim");
-				radnja();
-				((Label)labela).setText(this.toString());
-				labela.getParent().getParent().validate();
+		while (!interrupted()){
+			if (!radi) synchronized (this) {
+				while(!radi) wait();
 			}
+			System.out.println("Sacu da krenem da radim");
+			radnja();
+			((Label)labela).setText(this.toString());
+			labela.getParent().getParent().validate();
+		}
 		}catch(InterruptedException g){}
 	}
 	public void stani() { radi = false; }
